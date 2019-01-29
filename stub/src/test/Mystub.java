9baa9c605643094b26650156a722d6a2f303efe8
Alexandre <alexandre.ra@gmail.com>
2019-01-29 01:01:35 -0200

    stubs working and json file validations

--- stub/src/test/Mystub.java
+++ stub/src/test/Mystub.java
@@ -4,9 +4,8 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
 
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
@@ -20,26 +19,74 @@ public class Mystub {
 
 	@Rule
 	public WireMockRule wireMockRule = new WireMockRule(8888);
-	public String url = "http://www.urlficticia.com.br/api/v1/token/";
-	public String gerar_token1, content1, fileJson1 = "./src/test/gerar_token.json";
-//	public String gerar_token = "./src/test/gerar_token.json";
-//	public String gerar_token = "./src/test/gerar_token.json";
+	public String BaseURI = "http://www.urlficticia.com.br";
+	public String gerar_token, fileJson1 = "./src/test/gerar_token.json";
+	public String criar_emprestimo, fileJson2 = "./src/test/criar_emprestimo.json";
+	public String consultar_emprestimo, fileJson3 = "./src/test/consultar_emprestimo.json";
 
 	@Before
 	public void stubService() throws IOException {
-		gerar_token1 = new String(Files.readAllBytes(Paths.get(fileJson1)));
+		gerar_token = new String(Files.readAllBytes(Paths.get(fileJson1)));
+		criar_emprestimo = new String(Files.readAllBytes(Paths.get(fileJson2)));
+		consultar_emprestimo = new String(Files.readAllBytes(Paths.get(fileJson3)));
+
 		WireMock.configureFor("localhost", wireMockRule.port());
 		WireMock.reset();
-		stubFor(get(urlEqualTo("/api/v1/token/")).willReturn(aResponse().withStatus(200).withBody(gerar_token1)));
+		stubFor(get(urlEqualTo("/api/v1/token/")).willReturn(aResponse().withStatus(200).withBody(gerar_token)));
+		stubFor(get(urlEqualTo("/api/v1/emprestimos/"))
+				.willReturn(aResponse().withStatus(200).withBody(criar_emprestimo)));
+		stubFor(get(urlEqualTo("/api/v1/emprestimos/55208"))
+				.willReturn(aResponse().withStatus(200).withBody(consultar_emprestimo)));
 
-		System.out.println("breakpoint!!!");
+		// indicating that we are working with JSON format
+		RestAssured.defaultParser = Parser.JSON;
+
+		//
+
+		System.out.println("Set the breakpoint here to see the stubs working!");
 	}
 
 	@org.junit.Test
-	public void test() throws Exception {
+	public void serverIsUp() {
 
 		// Server is up
-		RestAssured.given().when().get("http://localhost:8888/api/v1/tokfen/").then().assertThat().statusCode(404);
+		RestAssured.given().when().get("http://localhost:8888/api/v1/token/").then().assertThat().statusCode(200);
+
+	}
+
+	@org.junit.Test
+	public void validateJsonFileGerarToken() {
+
+		// Json to be validate
+		System.out.println(gerar_token);
+
+		// Validating all the content included in the json file
+		RestAssured.given().when().get("http://localhost:8888/api/v1/token/").then().assertThat()
+				.content(Matchers.equalTo(gerar_token));
+
+	}
+
+	@org.junit.Test
+	public void validateJsonFileCriarEmprestimo() {
+
+		// Json to be validate
+		System.out.println(criar_emprestimo);
+
+		// Validating all the content included in the json file
+		RestAssured.given().when().get("http://localhost:8888/api/v1/emprestimos/").then().assertThat()
+				.content(Matchers.equalTo(criar_emprestimo));
+
+	}
+
+	@org.junit.Test
+	public void validateJsonFileConsultarEmprestimo() {
+
+		// Json to be validate
+		System.out.println(consultar_emprestimo);
+
+		// Validating all the content included in the json file
+		RestAssured.given().when().get("http://localhost:8888/api/v1/emprestimos/55208").then().assertThat()
+				.content(Matchers.equalTo(consultar_emprestimo));
 
 	}
 
